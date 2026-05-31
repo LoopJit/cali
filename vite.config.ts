aeb38f71fc069aa41016fd70c99fc9ce5101e96a
Sew <sewerjunk@gmail.com>
2026-05-31 03:57:28 +0000

    vite config

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: '/',
+    server: {
+        fs: {
+            allow: ['.']
+        }
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules/three')) return 'three'
+                    if (id.includes('node_modules/gsap')) return 'gsap'
+                }
+            }
+        }
+    },
+    assetsInclude: ['**/*.glb', '**/*.gltf']
+})
